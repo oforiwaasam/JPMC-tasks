55a5ba85a87a7f26d6aa44174be27b745f8635ff
Lily Sam <lilyoforiwaas@gmail.com>
2021-09-10 17:09:37 -0400

    Add files via upload

---
+++ JPMC-tech-task-3-py3/src/DataManipulator.ts
@@ -0,0 +1,36 @@
+/* this file will be responsible for processing the raw stock data from the server 
+ * before it throws it back to the Graph component's table to render*/
+import { ServerRespond } from './DataStreamer';
+
+// updating row to match schema in Graph
+// will be the structure of the return obj of generateRow function
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+}
+
+// this function will process the raw server data passed to it
+export class DataManipulator {
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.1;
+    const lowerBound = 1 - 0.1;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+    };
+  }
+}
---
+++ JPMC-tech-task-3-py3/src/Graph.tsx
@@ -0,0 +1,69 @@
+//this file takes care of how the Graph component of the App will be rendered
+//and react to the state changes that occur within the App
+import React, { Component } from 'react';
+import { Table } from '@jpmorganchase/perspective';
+import { ServerRespond } from './DataStreamer';
+import { DataManipulator } from './DataManipulator';
+import './Graph.css';
+
+interface IProps {
+  data: ServerRespond[],
+}
+
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void,
+}
+class Graph extends Component<IProps, {}> {
+  table: Table | undefined;
+
+  render() {
+    return React.createElement('perspective-viewer');
+  }
+
+  componentDidMount() {
+    // Get element from the DOM.
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+
+    //schema object will dictate how we'll be able to configure the Perspective table view of the graph
+    const schema = {
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float', // ratio field to track ratios of stocks
+      timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float', // moment when upper_bound and lower_bound are crossed
+    };
+
+    if (window.perspective && window.perspective.worker()) {
+      this.table = window.perspective.worker().table(schema);
+    }
+    if (this.table) {
+      // Load the `table` in the `<perspective-viewer>` DOM reference.
+      elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+      elem.setAttribute('aggregates', JSON.stringify({
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
+      }));
+    }
+  }
+
+  //method gets executed whenever the graph gets updated
+  componentDidUpdate() {
+    if (this.table) {
+      this.table.update([
+        DataManipulator.generateRow(this.props.data),
+      ]);
+    }
+  }
+}
+
+export default Graph;
